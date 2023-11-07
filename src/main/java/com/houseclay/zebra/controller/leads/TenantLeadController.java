3e226b9a09a64278393c0bd3191bf7299e1343e3
Gajanan Biradar <biradargajanan12345@gmail.com>
2023-11-07 18:11:33 +0530

    Added put mapping for edit the tenant lead

--- src/main/java/com/houseclay/zebra/controller/leads/TenantLeadController.java
+++ src/main/java/com/houseclay/zebra/controller/leads/TenantLeadController.java
@@ -2,8 +2,10 @@ package com.houseclay.zebra.controller.leads;
 
 import com.houseclay.zebra.dto.NewLeadTenantDTO;
 import com.houseclay.zebra.model.lead.LeadTenant;
+import com.houseclay.zebra.model.rental.PropertyRent;
 import com.houseclay.zebra.service.impl.TenantLeadServiceImpl;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -33,9 +35,25 @@ public class TenantLeadController {
 
     }
 
-    @GetMapping(value = "/get-lead/{id}")
     //get a specific details of a lead
-    public ResponseEntity<LeadTenant> getTenantLead(@PathVariable("id")UUID lead_id) {return null;}
+    @ApiOperation(value="Get Tenant Lead by Id",response = NewLeadTenantDTO.class)
+    @GetMapping(value = "/get-lead/{id}")
+    //public ResponseEntity<LeadTenant> getTenantLead(@PathVariable("id")UUID lead_id) {
+    public ResponseEntity<NewLeadTenantDTO> getTenantLead(@PathVariable("id")UUID lead_id) {
+        NewLeadTenantDTO newLeadTenantDTO=  this.tenantLeadService.findTenantLeadById(lead_id);
+        return  ResponseEntity.status(HttpStatus.OK)
+                .body(newLeadTenantDTO);
+    }
+
+    //Update Tenant lead by UUID
+    @ApiOperation(value="Update Tenant Lead by Id",response = NewLeadTenantDTO.class)
+    @PutMapping(value = "/edit/{tenant_lead_id}")
+    public ResponseEntity<NewLeadTenantDTO> upadteTenantLead(@RequestBody NewLeadTenantDTO newLeadTenantDTO,
+                                                             @PathVariable("tenant_lead_id")UUID lead_id) {
+        NewLeadTenantDTO newLeadTenant=  this.tenantLeadService.updateTenantLeadById(newLeadTenantDTO, lead_id);
+        return  ResponseEntity.status(HttpStatus.OK)
+                .body(newLeadTenant);
+    }
 
     //Get all leads with Pagegable Request
     @RequestMapping(value="/get-leads")
--- src/main/java/com/houseclay/zebra/service/TenantLeadService.java
+++ src/main/java/com/houseclay/zebra/service/TenantLeadService.java
@@ -1,16 +1,19 @@
 package com.houseclay.zebra.service;
 
 import com.houseclay.zebra.dto.NewLeadTenantDTO;
+import com.houseclay.zebra.dto.PropertyRentDTO;
 import com.houseclay.zebra.model.lead.LeadTenant;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 public interface TenantLeadService {
 
     Page<LeadTenant> findAllLeads(Pageable pageable);
     LeadTenant addTenantLead(NewLeadTenantDTO newLeadTenantDTO);
-
+    NewLeadTenantDTO findTenantLeadById(UUID uuid);
+    NewLeadTenantDTO updateTenantLeadById(NewLeadTenantDTO newLeadTenantDTO, UUID uuid);
 
 }
